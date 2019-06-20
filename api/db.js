a41ffaa61827dd01d2b1fe56d4306f14b53a7120
NareshKukreti12 <kukreti12.naresh@gmail.com>
2019-06-20 11:56:47 +0530

    Connection string changed

--- api/db.js
+++ api/db.js
@@ -1,9 +1,15 @@
 
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'admin123',
-    database :   'hackers_seach'
+// var connection = mysql.createConnection({
+//     host     : 'localhost',
+//     user     : 'root',
+//     password : 'admin123',
+//     database :   'hackers_seach'
+//   });
+  var connection = mysql.createConnection({
+    host     : '50.62.209.3',
+    user     : 'hackers_search',
+    password : '0Tx5r3$d',
+    database : 'hackers_search'
   });
   module.exports = connection;
